b891eea533e4971599973d5fdc28704d6c14a966
amruta dhawan <amrutadhawan@gmail.com>
2022-03-16 13:16:26 +0530

    javascript/level2

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,10 +3,39 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
-
+let collection=[numbers + userIds]; 
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
+let evenCollection=[];
 
+for(let number of numbers){
+    if(number%2 === 0){
+        evenCollection.push(number);
+    }
+}
+console.log(evenCollection);
+
+for(let userId of userIds){
+  if( userId%2 === 0){
+      evenCollection.push(userId);
+  }
+}
+console.log(evenCollection);
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
+let oddCollection=[];
+
+for(let number of numbers){
+    if(number%2 === 0){
+        oddCollection.push(number);
+    }
+}
+console.log(oddCollection);
+
+for(let userId of userIds){
+  if( userId%2 === 0){
+      oddCollection.push(userId);
+  }
+}
+console.log(oddCollection);
 
 /*
   @param means parameter
@@ -24,10 +53,13 @@ let userIds = [1230, 234, 1278, 984, 763, 900];
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
 
-function times() {
+function times(number,string) {
+  for(let i=0;i<number;i++){
+    number+=string;
+  }
   // Your code
 }
-
+console.log(times(5, 'c'));
 // Uncomment the code below and test the output
 
 // console.log(times(5, 'c')); // ['c', 'c', 'c', 'c', 'c']
@@ -48,10 +80,19 @@ function times() {
     revert(['Ryan', 'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function revert() {
-  // your code
-}
 
+
+function revert (arr) {
+  let numArr = arr.length;   
+ 
+  let newArr = [];    
+  for (i = 0; i < numArr; i++) {      
+      let inArr = arr;
+      newArr[i] = inArr.pop(i);       
+  }
+  return newArr;
+}
+console.log(revert([1, 2, 3, 4]));
 // Uncomment the code below and test the output
 // console.log(revert([1, 2, 3, 4])); // [4, 3, 2, 1]
 // console.log(revert(['a', 'd', 'c', 'b'])); // ['b', 'c', 'd', 'a']
@@ -70,10 +111,16 @@ function revert() {
     clear(['Ryan', null, 0,  'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function clear() {
+function clear(element) {
   // your code
+  let newArr=[];
+  if(element !== undefined && element!== null &&  element !== 0){
+    
+    newArr=element;
+  }
+  return newArr;
 }
-
+console.log(clear([1, 2, 3, 4, '', 0, null, undefined]));
 // Uncomment the code below and test the output
 // console.log(clear([1, 2, 3, 4, '', 0, null, undefined])); // [4, 3, 2, 1]
 // console.log(clear(['a', undefined, 'd', 0, 'c', 'b'])); // ['b', 'c', 'd', 'a']
@@ -81,7 +128,7 @@ function clear() {
 
 /*
 
- 6. Write a function named arrayToObj which accepts an array and return an object
+ 7. Write a function named arrayToObj which accepts an array and return an object
  where the key will be the index of array and value will be the element of the array.
 
   @param arr (array)
@@ -93,10 +140,13 @@ function clear() {
     arrayToObj(['Ryan', 'John']); // {0: 'Ryan', 1: 'John'}
 */
 
-function arrayToObj() {
+function arrayToObj(arr) {
   // your code
+  let index=arr.length-1;
+  let newArr=index;
+  return newArr;
 }
-
+console.log(arrayToObj([1, 2, 3, 4]));
 // Uncomment the code below and test the output
 // console.log(arrayToObj([1, 2, 3, 4])); // {0: 1, 1: 2, 2: 3, 3: 4}
 // console.log(arrayToObj(['a', undefined, 'd'])); // {0: 'a', 1: undefined, 2: 'd'}
